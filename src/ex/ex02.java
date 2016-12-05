c4f3f5c987bc4124ee0da73e186eb7c5888d9b35
1 <1>
2016-12-05 15:44:49 +0800

    1 3not yet

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,35 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 輸入所使用的度數，換算夏月及非夏月之電費金額。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021033 黃苡珊
  */
 
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int v1 = scn.nextInt();
+		if(v1<=120){
+			System.out.println(v1*2.1);
+			System.out.println(v1*2.1);
+		}else if(v1>=121&&v1<=330){
+			System.out.println(120*2.1+(v1-120)*3.02);
+			System.out.println(120*2.1+(v1-120)*2.68);
+		}else if(v1>=331&&v1<=500){
+			System.out.println(120*2.1+(330-120)*3.02+(v1-330)*4.39);
+			System.out.println(120*2.1+(330-120)*2.68+(v1-330)*3.61);
+		}
+			else if(v1>=501&&v1<=700){
+				System.out.println(120*2.1+(330-120)*3.02+(500-330)*4.39+(v1-500)*4.97);
+				System.out.println(120*2.1+(330-120)*2.68+(500-330)*3.61+(v1-500)*4.01);
+		}
+			else if(v1>=701){
+				System.out.println(120*2.1+(330-120)*3.02+(500-330)*4.39+(700-500)*4.97+(v1-700)*5.63);
+				System.out.println(120*2.1+(330-120)*2.68+(500-330)*3.61+(700-500)*4.01+(v1-700)*4.5);
+		}
 	}
-
 }
--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,14 +2,29 @@
 /*
  * Topic: 請設計 Person 類別 (姓名,性別,身高,體重)，使用者可以新增個人資料，每新增一筆就會列出目前的資料串有哪些人，而這些資料請用插入排序法依每一個人的 BMI 值由大到小排序。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021033 黃苡珊
  */
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		ArrayList<Integer>dataList=new ArrayList<Integer>();
+		String str ="r";
+		int i = 0,val=0;
+		boolean flag=false;
+		while(!(str.equals("N")||(str.equals("n"))){
+			System.out.println("input a value:");
+			val= scn.nextInt();
+		
+		
+		
+		
+		}
+//		int v1 = scn.nextInt();  
 	}
 
 }

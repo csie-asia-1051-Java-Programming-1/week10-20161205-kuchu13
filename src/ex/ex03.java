3c0c1cfd76178915c2cabc16086d0cd50487cfaa
kuchu13 <105021033@live.asia.edu.tw>
2017-01-03 22:15:41 +0800

    1 123

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -12,19 +12,52 @@ public class ex03 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		ArrayList<Integer>dataList=new ArrayList<Integer>();
-		String str ="r";
-		int i = 0,val=0;
-		boolean flag=false;
-		while(!(str.equals("N"))||(str.equals("n"))){
+		ArrayList<Integer> dataList = new ArrayList<Integer>();
+		String str = "Y";
+		int i = 0, val = 0;
+		boolean flag = false;
+		while (!(str.equals("N")) || (str.equals("n"))) {
 			System.out.println("input a value:");
-			val= scn.nextInt();
-		
-		
-		
-		
+			val = scn.nextInt();
+			if (dataList.size() == 0) {
+				dataList.add(val);
+			} else if (dataList.size() == 1) {
+				if (dataList.get(i) > val) {
+					dataList.add(val);
+				} else {
+					dataList.add(0, val);
+				}
+			} else {
+				flag = true;
+				i = 0;
+				while (flag && i < dataList.size() - 1) {
+					if (i == 0 && dataList.get(0) < val) {
+						dataList.add(0, val);
+						flag = false;
+					} else {
+						if (dataList.get(i) > val && dataList.get(i + 1) <= val) {
+							flag = false;
+							dataList.add(i + 1, val);
+						} else {
+							i++;
+						}
+					}
+				}
+			}
+			showListData(dataList);
+			System.out.print("Continue?");
+			str = scn.next();
 		}
-//		int v1 = scn.nextInt();  
+
+	}
+
+	private static void showListData(ArrayList<Integer> List) {
+		int size = List.size();
+		for (int i = 0; i < size; i++) {
+			System.out.print(List.get(i) + "\t");
+		}
+		System.out.println();
+
 	}
 
 }

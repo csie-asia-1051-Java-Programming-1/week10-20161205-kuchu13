4c468cc1c841166dd6d5d6f3679a43b1e6fd9401
kuchu13 <105021033@live.asia.edu.tw>
2017-01-04 00:51:32 +0800

    1 1

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,48 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int i = 0;
+		int v1 = scn.nextInt();
+		int v2 = scn.nextInt();
+		int v3 = scn.nextInt();
+		int v4 = scn.nextInt();
 
+		if (v1 == v2 || v2 == v3 || v3 == v4 || v4 == v1 || v1 == v3 || v2 == v4) {
+			while (true) {
+				if (v1 == v2 && v3 == v4 && v2 == v3 && v1 == v4) {
+					while (true) {
+						System.out.print("通殺");
+						break;
+					}
+					break;
+				} else if (v1 == v2) {
+					i = v3 + v4;
+					break;
+				} else if (v1 == v3) {
+					i = v2 + v4;
+					break;
+				} else if (v1 == v4) {
+					i = v2 + v3;
+					break;
+				} else if (v2 == v3) {
+					i = v1 + v4;
+					break;
+				} else if (v2 == v4) {
+					i = v1 + v3;
+					break;
+				} else if (v3 == v4) {
+					i = v1 + v2;
+					break;
+				}
+				System.out.print(i);
+			}
+		} else {
+			System.out.print("無意義");
+		}
 	}
-
 }
--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,10 +1,42 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();//n個人
+		int l, r, s, t;
+		for (int i = 0; i < n; i++) {
+			System.out.print("聽力");
+			l = scn.nextInt();
+			
+			System.out.print("閱讀");
+			r = scn.nextInt();
+			
+			System.out.print("口說");
+			s = scn.nextInt();
+			t = l + r + s;//總分
+			
+			if (l >= 60 && r >= 60 && s >= 60 && t <= 300) {
+				System.out.println("P");
+				
+			} else if (l < 60 && t > 220 || r < 60 && t > 220 || s < 60 && t > 220) {
+				System.out.println("P");
+				
+			} else if (l < 60 && r >= 60 && s >= 60 && t < 220|| l >= 60 && r < 60 && s >= 60 && t < 220|| l >= 60 && r >= 60 && s < 60 && t < 220) {
+				System.out.println("M");
+				
+			} else if (l < 60 && r < 60 && s >= 80 || l < 60 && r >= 80 && s < 60|| l >= 80 && r < 60 && s < 60) {
+				System.out.println("M");
+				
+			} else if (l < 0 || l > 100 || r < 0 || r > 100 || s < 0 || s > 100) {
+				System.out.println("請輸入0~100分");
+				
+			} else {
+				System.out.println("F");
+			}
+		}
 	}
-
 }
